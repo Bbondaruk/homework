bfa389ccc2267ea6dc7a9a6eb3f0bb324e7ea9d1
Bbondaruk <mega.bondaruk@gmail.com>
2023-06-19 11:07:19 +0300

    Covered case with 1000m for task 9

--- hw.js
+++ hw.js
@@ -184,7 +184,7 @@ console.log(vowels.replace(/[aeiou]/g, ''))
 
     // Підказка(https://ukr-lifehacks.ed-era.com/rozdil-9/zvyazok_chislivnykiv)
 
-let m = 12;
+let m = 1000;
 let km = m / 1000;
 
 if (m == 0) {
@@ -200,8 +200,10 @@ if (m == 0) {
     } else {
         console.log(m, 'метрів це', km, 'кілометра')
     }
-} else if(m.toString().endsWith('11') || m.toString().endsWith('12') || m.toString().endsWith('13') || m.toString().endsWith('14')) {
+} else if (m.toString().endsWith('11') || m.toString().endsWith('12') || m.toString().endsWith('13') || m.toString().endsWith('14')) {
     console.log(m, 'метрів це', km, 'кілометрів')
+} else if (km == 1) {
+    console.log(m, 'метрів це', km, 'кілометр')
 }
 else if (m.toString().endsWith('1')) {
     console.log(m, 'метр це', km, 'кілометр')
